cd313326c27e226e2884d5c4e0c32cd6f0d3d7e3
ashwanikumar <ashwanikumar01011997123@gmail.com>
2023-05-12 21:13:33 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// ajax libarary used
+$(document).ready(function () {
+    $('.menu-btn').click(function(){
+       $('.navbar .menu').toggleClass("active");
+       $('.menu-btn i').toggleClass("active");
+   });
+  });
+
+
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+      // sticky navbar on scroll script
+      if (this.scrollY > 20) {
+        document.querySelector('.navbar').classList.add("sticky");
+      } else {
+        document.querySelector('.navbar').classList.remove("sticky");
+      }
+  
+      // scroll-up button show/hide script
+      if (this.scrollY > 500) {
+        document.querySelector('.scroll-up-btn').classList.add("show");
+      } else {
+        document.querySelector('.scroll-up-btn').classList.remove("show");
+      }
+    });
+  
+    // slide-up script
+    document.querySelector('.scroll-up-btn').addEventListener("click", function() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      // removing smooth scroll on slide-up button click
+      document.documentElement.style.scrollBehavior = "auto";
+    });
+  
+    var menuLinks = document.querySelectorAll('.navbar .menu li a');
+    menuLinks.forEach(function(link) {
+      link.addEventListener("click", function() {
+        // applying again smooth scroll on menu items click
+        document.documentElement.style.scrollBehavior = "smooth";
+      });
+    });
+});
