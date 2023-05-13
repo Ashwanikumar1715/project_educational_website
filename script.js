877bac0c52a260f95f808b2b37884f2d954e73ee
ashwanikumar <ashwanikumar01011997123@gmail.com>
2023-05-13 14:25:18 +0530

    new change

--- script.js
+++ script.js
@@ -9,9 +9,30 @@ $(document).ready(function () {
 
 
 
+let slideIndex = 1;
+showSlide(slideIndex);
 
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
 
-
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -45,4 +66,6 @@ document.addEventListener("DOMContentLoaded", function() {
       document.documentElement.style.scrollBehavior = "smooth";
     });
   });
+
+
 });
